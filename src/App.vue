32905b76a3aaa2901633bfe067f007bb7dcbcc3e
Katherine Lewis <kale8432@colorado.edu>
2019-08-23 17:38:00 -0600

    created vue app, building app component, creating file strcuture

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Form/>
+    <TodoContainer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Form from './components/Form.vue'
+import TodoContainer from './components/TodoContainer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Form,
+    TodoContainer
+  }, 
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+* {
+   box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  }
+
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-image: url('./assets/lauren-sauder-6GPBVYLapYQ-unsplash.jpg');
+  background-repeat: no-repeat;
+}
+</style>
